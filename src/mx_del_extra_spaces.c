0b77d4a31674f402fb13f9b08147dfed83c17bf8
Klymok Nazariy <klymok.nazariy@gmail.com>
2021-03-04 19:26:27 +0200

    fixed mx_del_extra_spaces sigsegv

--- src/mx_del_extra_spaces.c
+++ src/mx_del_extra_spaces.c
@@ -1,10 +1,13 @@
 #include "libmx.h"
 
+#include <stdio.h>
 int calc_len_clean(const char *s) {
 	int count = 0;
 	bool had_space = false;
 
-	for (int i = 0; s[i] != '\0'; i++) {
+	if (!s)
+	    return -1;
+	for (int i = 0; s[i]; i++) {
 		if (!had_space || !mx_isspace(s[i]))
 			count++;
 		had_space = mx_isspace(s[i]);
@@ -21,8 +24,11 @@ char *mx_del_extra_spaces(const char *str) {
         return NULL;
     trimmed = mx_strtrim(str);
     result = mx_strnew(calc_len_clean(trimmed));
-	if (!trimmed || !result)
-		return NULL;
+	if (!trimmed || !result) {
+	    mx_strdel(&trimmed);
+        mx_strdel(&result);
+        return mx_strnew(1);
+    }
 	had_space = false;
 	for (int i = 0, j = 0; trimmed[i] != '\0'; i++) {
 		if (!had_space || !mx_isspace(trimmed[i]))
